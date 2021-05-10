c9698837ddc84104b4837849cd076d5116c15140
Rafael Rakochinski Camargo <66796237+RafaelRCamargo@users.noreply.github.com>
2021-05-10 19:17:41 +0000

    first commit
    uploading thm-autoreadme

---
+++ README.md
@@ -0,0 +1,25 @@
+# TryHackMe Auto README.md generator
+
+This is a super-simple and lightweight tool to help you speed up your CTF documentation process.
+
+> May, 2021 
+
+---
+
+Use the pip to **install** the **TryHackMe api**:
+
+````commandline
+pip install thmapi
+````
+
+Now you just need to choose the **room** and **run the command**:
+
+````commandline
+python autoreadme.py vulnversity
+````
+
+- Plz note: 
+  - ```python``` - calls **python**.
+  - ```autoreadme.py``` - it's the **main script**.
+  - ```vulnversity``` - it's the **name of the room**, can be changed for any other room.    
+    _- **Remember this name needs to be equals to de url**, usually the room name in lowercase_
---
+++ Thm-AutoReadme/autoreadme.py
@@ -0,0 +1,53 @@
+import os
+import re
+import sys
+import getpass
+from thmapi import THM
+from datetime import date
+
+# API
+thm = THM()
+
+# Room details
+room = thm.room_details(sys.argv[1])
+room_questions = thm.room_tasks(sys.argv[1])
+
+# Date
+today = date.today()
+# Textual month, day and year
+date = today.strftime("%b %d, %Y")
+
+
+def cleanhtml(raw_html):
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', raw_html)
+    return cleantext
+
+
+path = room.get('title')
+
+try:
+    os.mkdir(path)
+except OSError:
+    print("Creation of the directory %s failed" % path)
+else:
+    print("Successfully created the directory %s " % path)
+
+file = open(f"{path}\README.md", "w")
+file.write(f"# TryHackMe {room.get('type')}\n\n"
+           f"## {room.get('title')}\n\n"
+           f"> _{getpass.getuser()} | {date}_\n\n")
+
+for i in room_questions:
+    # print(cleanhtml((str(i.get('questions')))), "\n")
+    for ii in i.get('questions'):
+        if ii.get('questionNo') == 1:
+            file.write(f"----------------------------------------\n\n")
+            file.write(f"{ii.get('questionNo')}. {cleanhtml(ii.get('question'))}\n\n")
+        else:
+            file.write(f"{ii.get('questionNo')}. {cleanhtml(ii.get('question'))}\n\n")
+        file.write("```\n")
+        file.write("")
+        file.write("```\n\n")
+
+file.close()
